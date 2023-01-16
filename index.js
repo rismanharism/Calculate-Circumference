9b24b3d1d90d2fffcf90bd9d580444cee6b4610c
rismanharism <risman.haris6@gmail.com>
2023-01-16 20:43:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// Insert your code here
+var pi = Math.PI;
+var jariJari = 5;
+
+let hasil = pi * (jariJari * jariJari)
+console.log(hasil);
+/*
+algorittma
+memasukan nilai π
+memasukan nilai r
+keliling lingkaran adalah 2 di kali π
+menumakan hasil 2 di kali π
+hasil 2 di kali π di kali lagi denga r
+ditemukan hasil keliling lingkaran
+
+pseudocode
+STORE pi AS NUMBER WITH 3,14
+STORE jari2 AS NUMBER ANY VALUE
+STORE kelilingLingkaran 0
+CALCULATE 2 TIMES 3,14 TIMES jari2
+SET kelilingLingkaran VALUE WITH CALCULATION RESULT
+DISPLAY kelilingLingkaran
+
+**/
